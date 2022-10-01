d61fabf9fb9d550236fa7ecb99ff43ddeee56421
Bekha555 <bekha.mulloakhmedov@gmail.com>
2022-10-02 00:06:37 +0500

    new

--- app/src/main/java/com/example/beta1/SaveData.kt
+++ app/src/main/java/com/example/beta1/SaveData.kt
@@ -1,9 +1,13 @@
 package com.example.beta1
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,6 +17,7 @@ import com.example.beta1.utils.SessionManager
 import kotlinx.android.synthetic.main.activity_save_data.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -21,9 +26,10 @@ class SaveData : AppCompatActivity() {
     private lateinit var adapterOf: rvAdapter
     private lateinit var sessionManager: SessionManager
     private lateinit var button: Button
-
+    private lateinit var dialog: Dialog
     private lateinit var database: WaterDataBase
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_data)
@@ -40,22 +46,51 @@ class SaveData : AppCompatActivity() {
             recycle.adapter = rvAdapter(dataList)
         }
 
+        1
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialog)
+        dialog.getWindow()?.setBackgroundDrawable(getDrawable(R.drawable.background))
+        dialog.getWindow()
+            ?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.setCancelable(true)
+        dialog.getWindow()?.getAttributes()?.windowAnimations = R.style.animation
+
+
+        val okButton = dialog.findViewById<Button>(R.id.btn_yes)
+        val noButton = dialog.findViewById<Button>(R.id.btn_no)
+
+        okButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                delay(1500)
+                database.waterDao().clearData()
+
+            }
+            Toast.makeText(this, "Очищено", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        noButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
 
         clearButton.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Очистить все чеки?")
-            builder.setMessage("Вы больше не сможите восстановить их!")
-            builder.setNegativeButton("Нет") { dialog, i ->
+            dialog.show()
 
-            }
-            builder.setPositiveButton("Да") { dialog, i ->
-                CoroutineScope(Dispatchers.IO).launch {
 
-                    database.waterDao().clearData()
-
-                }
-            }
-            builder.show()
+//           val builder = AlertDialog.Builder(this)
+//            builder.setTitle("Очистить все чеки?")
+//            builder.setMessage("Вы больше не сможите восстановить их!")
+//            builder.setNegativeButton("Нет") { dialog, i ->
+//
+//            }
+//            builder.setPositiveButton("Да") { dialog, i ->
+//                CoroutineScope(Dispatchers.IO).launch {
+//
+//                    database.waterDao().clearData()
+//
+//                }
+//            }
+//            builder.show()
         }
 
 
@@ -83,9 +118,6 @@ class SaveData : AppCompatActivity() {
         button.setOnClickListener {
             onBackPressed()
         }
-
-
-
 
 
     }
